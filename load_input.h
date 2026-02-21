00e6ec5122ce232492329ae8a683fca5786ca881
appMelon6 <ee24b066@smail.iitm.ac.in>
2026-02-22 01:21:10 +0530

    Update load_input.h

--- load_input.h
+++ load_input.h
@@ -2,17 +2,18 @@
 
 void load_input(
     data_f *input,
-    data_f input_buffer[28][28],
     data_f *weight1,
-    data_f W1[8][3][3],
-    data_f *weight2,
-    data_f W2[16][8][3][3],
-    data_f *weight3,
-    data_f W3[10][400],
     data_f *bias1,
-    data_f B1[8],
+    data_f *weight2,
     data_f *bias2,
-    data_f B2[16],
+    data_f *weight3,
     data_f *bias3,
-    data_f B3[10]
+
+    hls::stream<data_f> &s_input,
+    hls::stream<data_f> &s_w1,
+    hls::stream<data_f> &s_b1,
+    hls::stream<data_f> &s_w2,
+    hls::stream<data_f> &s_b2,
+    hls::stream<data_f> &s_w3,
+    hls::stream<data_f> &s_b3
 );
