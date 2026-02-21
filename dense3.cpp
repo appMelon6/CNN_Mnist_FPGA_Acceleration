9de5e1dbf9bdfa49bd4b40157b24f17981827546
appMelon6 <ee24b066@smail.iitm.ac.in>
2026-02-22 01:19:35 +0530

    Update dense3.cpp

--- dense3.cpp
+++ dense3.cpp
@@ -2,57 +2,73 @@
 
 void dense3(
     hls::stream<vec16_f> &input,
-    data_f weight[10][400],
-    data_f bias[10],
-    data_f output[10]
+    hls::stream<data_f>  &weight_stream,
+    hls::stream<data_f>  &bias_stream,
+    hls::stream<data_f>  &output_stream
 )
 {
 #pragma HLS INLINE off
 
-#pragma HLS ALLOCATION operation instances=mul limit=48
+//#pragma HLS ALLOCATION operation instances=mul limit=48
 
+    data_f weight[10][400];
+    data_f bias[10];
+
+#pragma HLS ARRAY_PARTITION variable=weight dim=2 cyclic factor=16
 #pragma HLS ARRAY_PARTITION variable=bias dim=0 type=complete
-#pragma HLS ARRAY_PARTITION variable=output dim=0 type=complete
-#pragma HLS ARRAY_PARTITION variable=weight dim=2 type=cyclic factor=16
+
+    Loading_weights_buffer:
+    for (int o = 0; o < 10; o++) {
+        for (int i = 0; i < 400; i++) {
+#pragma HLS PIPELINE II=1
+            weight[o][i] = weight_stream.read();
+        }
+    }
+
+    Loading_bias_buffer:
+    for (int o = 0; o < 10; o++) {
+#pragma HLS PIPELINE II=1
+        bias[o] = bias_stream.read();
+    }
 
     acc_f acc[10];
 #pragma HLS ARRAY_PARTITION variable=acc complete
 
-    // Initialize accumulators with bias
+    Initializing_accumulator:
     for (int o = 0; o < 10; o++) {
 #pragma HLS UNROLL
         acc[o] = (acc_f)bias[o];
     }
 
-    // 400/16 (parallelization)
+    Dense_layer:
     for (int i = 0; i < 25; i++) {
-#pragma HLS PIPELINE II=10
 
         vec16_f pix = input.read();
 
 #pragma HLS ARRAY_PARTITION variable=pix.ch complete
 
-        // For each input channel
+        Looping_over_Input_channels:
         for (int ic = 0; ic < 16; ic++) {
-#pragma HLS UNROLL
+#pragma HLS PIPELINE II=1
 
             data_f val = pix.ch[ic];
             int base = i*16;
 
-            // Update all outputs
+            Updating_outputs:
             for (int o = 0; o < 10; o++) {
+#pragma HLS UNROLL
                 acc[o] += (acc_f)val * weight[o][base + ic];
             }
 
         }
     }
 
-    // Write final outputs
+    Writing_output:
     for (int o = 0; o < 10; o++) {
-#pragma HLS UNROLL
+#pragma HLS PIPELINE II=1
 
         acc_f temp = acc[o];
 
-        output[o] = (data_f)temp;
+        output_stream.write((data_f)temp);
     }
 }
