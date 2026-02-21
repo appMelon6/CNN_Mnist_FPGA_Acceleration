df8a27277c1e4a9fd05ae22db194cd6fd6718608
appMelon6 <ee24b066@smail.iitm.ac.in>
2026-02-22 01:17:43 +0530

    Update conv2.h

--- conv2.h
+++ conv2.h
@@ -1,8 +1,8 @@
 #include "data_types.h"
 
 void conv2(
-    hls::stream<vec8_f> &input,
-    data_f weight[16][8][3][3],
-    data_f bias[16],
+    hls::stream<vec8_f>  &input,
+    hls::stream<data_f>  &weight_stream,
+    hls::stream<data_f>  &bias_stream,
     hls::stream<vec16_f> &output
 );
